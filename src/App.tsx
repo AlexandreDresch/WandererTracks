2825c077560b8bb9e67e887964e638076ffbb66c
AlexandreDresch <connectWithAlexandre@proton.me>
2024-04-19 23:37:20 -0300

    feat: add header to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Pricing from "./pages/pricing";
 import NotFound from "./pages/not-found";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <BrowserRouter>
+      <div className="w-full h-screen px-8">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      </div>
     </BrowserRouter>
   );
 }
